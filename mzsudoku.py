42372863d95edbca42ad47578c29e7cf5c8f298a
Zvolský <mirek.zvolsky@gmail.com>
2013-12-03 17:06:24 +0100

    first apply_known

--- mzsudoku.py
+++ mzsudoku.py
@@ -5,22 +5,50 @@
 import sys
 import itertools
 
+finished = False
+failure = False
+
 def same_row(i,j): return (i/9 == j/9)
 def same_col(i,j): return (i-j) % 9 == 0
 def same_block(i,j): return (i/27 == j/27 and i%9/3 == j%9/3)
 
-def run(zadani, out='pnr'):
-    '''run all: mark possibilities(p), find n-combinations(n), find result(r)'''
-    marked = p(sys.argv[1])  # mark possibilities
-    if 'p' in out:
-        pp(marked)           # print possibilities as table
-    if 'n' in out:
-        n(marked)            # localize hidden n-combinations
-    if 'r' in out:
-        r(sys.argv[1])       # solve based on original recursive algorithmus
+def run(setting, out='pnr'):
+    '''run all: mark possibilities(p), find n-combinations(n), find result(r)
+    in addition to pnr, out cat contain:
+     = show definitely known numbers (otherwise show only unknown-possibilities)
+     A apply known new numbers and cycle again
+     a apply known new numbers after pressing Enter (anything else will quit)
+    
+    TODO: for a/A, ie. in apply_known, at this time naked singletons will apply
+      only. We need to apply hidden combinations (obtained from n()) at least 
+    '''
+    while True:
+        marked = p(setting)          # mark possibilities
+        if 'p' in out:
+            pp(marked, '=' in out)  # print possibilities as table
+        if 'n' in out:
+            n(marked)               # localize hidden n-combinations
+        if 'r' in out:
+            r(setting)           # solve based on original recursive algorithmus
+        if finished:
+            print
+            print "FINISHED."
+        if failure:
+            print
+            print "FAILURE IN SETTING."
+        if not 'a' in out.lower():
+            break
+        setting2 = apply_known(setting, marked)
+        if setting==setting2:
+            break
+        if 'a' in out and raw_input(): 
+            break
+        setting = setting2
 
 def p(a):
     '''mark possibilities'''
+    global finished
+    finished = True
     marked = []
     for i in xrange(81):
         if a[i]=='0':
@@ -30,12 +58,14 @@ def p(a):
                           same_row(i,j) or same_col(i,j) or same_block(i,j)):
                     excluded_numbers.add(a[j])
             marked.append(excluded_numbers)
+            finished = False
         else:
             marked.append(a[i])
     return marked
 
 def n(marked):
     '''print where n-sets are inside possibilities'''
+    first = True
     for ln in xrange(9):
         block = []
         excluded = []
@@ -45,7 +75,7 @@ def n(marked):
                 block.append(marked[pos])
             else:
                 excluded.append(marked[pos])
-        solveN('ln%s'%(ln+1), block, excluded)         
+        first = solveN('ln%s'%(ln+1), block, excluded, first)         
     for cl in xrange(9):
         block = []
         excluded = []
@@ -55,7 +85,7 @@ def n(marked):
                 block.append(marked[pos])
             else:
                 excluded.append(marked[pos])
-        solveN('cl%s'%(cl+1), block, excluded)         
+        first = solveN('cl%s'%(cl+1), block, excluded, first)         
     for bl in xrange(9):
         hor = bl/3
         vert = bl%3 
@@ -68,10 +98,11 @@ def n(marked):
                     block.append(marked[pos])
                 else:
                     excluded.append(marked[pos])
-        solveN('bl%s'%(bl+1), block, excluded)
+        first = solveN('bl%s'%(bl+1), block, excluded, first)
 
-def solveN(symbol, block, excluded):
+def solveN(symbol, block, excluded, first):
     #print symbol, len(block), excluded
+    global failure
     included = []
     for number in '123456789':
         if not number in excluded:
@@ -87,24 +118,31 @@ def solveN(symbol, block, excluded):
                 if len(where)>lenN:
                     break
             else:
+                if first:
+                    print "hidden n-combinations"
+                    print ("[where / which numbers / field position "
+                            "(from unknown only)]:")
+                    first = False
                 print symbol, candidate, where,
                 if len(where)<lenN:
                     print 'WARNING: in %s fields only' % len(where)
+                    failure = True
                 else:
                     print
+    return first  
 
-def pp(marked):
+def pp(marked, show_fixed=False):
     '''print marked possibilities'''
     for ln in xrange(36):
         row = ln/4
         subln = ln%4 
         if subln:
-            psub(subln, row, marked)
+            psub(subln, row, marked, show_fixed)
         else:
             ppl()
     ppl()
 
-def psub(subln, row, marked):
+def psub(subln, row, marked, show_fixed=False):
     for cl in xrange(36):
         subcl = cl%4
         if subcl:
@@ -117,7 +155,7 @@ def psub(subln, row, marked):
                     pppossible(subnum) 
             elif subcl==1:
                 if subln==2:
-                    ppfixed(marked[pos])
+                    ppfixed(marked[pos], show_fixed)
                 else:
                     ppspace()
         else:
@@ -140,10 +178,13 @@ def ppvertsep():
 def pplinesep():
     print
 
-def ppfixed(symbol):
-    #print '* %s *' % symbol,
-    #print ' _%s_ ' % symbol,
-    ppspace()
+def ppfixed(symbol, show_fixed=False):
+    if show_fixed:
+        #print '* %s *' % symbol,
+        #print ' _%s_ ' % symbol,
+        print ' = %s ' % symbol,
+    else:
+        ppspace()
 
 def ppspace():
     print 5*' ',
@@ -154,7 +195,8 @@ def r(a):
     mz change: #mz'''
     i = a.find('0')
     if i == -1:
-        sys.exit(a)
+        print a
+        return
   
     excluded_numbers = set()
     for j in xrange(81):
@@ -166,6 +208,18 @@ def r(a):
     for m in '123456789':
         if m not in excluded_numbers:
             r(a[:i]+m+a[i+1:])
+
+def apply_known(setting, marked):
+    setting2 = ''
+    for i, fld in enumerate(marked):
+        if len(fld)==8:
+            for number in '123456789':
+                if not number in fld:
+                    setting2 += number
+                    break
+            continue   # setting2 enlarged already
+        setting2 += setting[i]
+    return setting2 
 
 if __name__ == '__main__':
     if len(sys.argv)>=2 and len(sys.argv[1]) == 81:
